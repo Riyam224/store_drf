63b52993bf6cb65da3164338e60aee672bff785b
Riyam224 <cs50riyam211#gmail.com>
2023-12-05 21:25:34 +0300

    second

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = ["127.0.0.1", ".onrender.com"]
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
